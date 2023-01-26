eedad61e992f87750ffe5d2a09e1ec798b2fb45f
Mateo Guillen <mmateoguillen@gmail.com>
2023-01-26 17:21:05 -0500

    Validacion en equipos, formacion de grupos.

--- bolirrana/app2.js
+++ bolirrana/app2.js
@@ -3,14 +3,20 @@ const contenedorBlur = document.querySelector(".contenedor-blur")
 
 const blur = document.querySelector(".blur")
 
+// pantalla inicial:
+
 const comoJugar = document.querySelector(".h3-comojugar")
 const parrafo = document.querySelector(".parrafo-introduccion")
 const boton = document.querySelector(".boton-empezae")
 const titulo = document.querySelector(".h2-bolirrana")
 
+// Modo de juego:
+
 const escogerModoDeJuegoH2 = document.createElement("h2");
 const botonCasual = document.createElement("button")
 const boton_competitivo = document.createElement("button")
+
+// Modo de agrupacion:
 
 const divOpciones = document.createElement("div")
 const escogerModoDeGruposH2 = document.createElement("h2")
@@ -18,17 +24,22 @@ const botonIndividual = document.createElement("button")
 const botonEquipos = document.createElement("button")
 const botonEquiposACiegas = document.createElement("button")
 
-divOpciones.appendChild(botonIndividual)
-divOpciones.appendChild(botonEquipos)
-divOpciones.appendChild(botonEquiposACiegas)
-console.log(divOpciones.childNodes)
+// Numero de jugadores: 
 
 const h2EligeNumeroDeJugadores = document.createElement("h2")
 const inputNumeroDeJugadores = document.createElement("input")
 const botonSiguiente = document.createElement("button")
 
+// Numero de jugadores por equipo:
+
+const h2EligeNumeroDeJugadoresPorEquipo = document.createElement("h2")
+const input_numero_jugadores_por_equipo = document.createElement("input")
+
+// Alerta:
+
 const alerta = document.createElement("p")
 
+// Boton introduccion:
 
 boton.addEventListener("click",()=>{
     titulo.classList.add("animate__backOutUp")
@@ -62,6 +73,8 @@ boton.addEventListener("click",()=>{
     }, 250) 
 
 })
+
+// Botones:
 
 botonCasual.addEventListener("click",()=>{
     botonCasual.classList.replace("animate__backInUp","animate__backOutRight")
@@ -186,7 +199,7 @@ botonIndividual.addEventListener("click",()=>{
 })
 
 botonEquipos.addEventListener("click",()=>{
-    botonIndividual.classList.replace("animate__backInUp","animate__backOutRight")
+    botonIndividual.classList.replace("animate__backInUp","animate__backOutDown")
     botonEquipos.classList.replace("animate__backInUp","animate__backOutRight")
     botonEquiposACiegas.classList.replace("animate__backInUp","animate__backOutDown")
     escogerModoDeGruposH2.classList.replace("animate__backInDown","animate__backOutUp")
@@ -202,10 +215,19 @@ botonEquipos.addEventListener("click",()=>{
         h2EligeNumeroDeJugadores.classList.add("elije_numero_jugadores_equipos")
         h2EligeNumeroDeJugadores.innerHTML="Escriba el numero de jugadores: "
 
+        document.querySelector(".blur").appendChild(h2EligeNumeroDeJugadoresPorEquipo)
+        h2EligeNumeroDeJugadoresPorEquipo.classList.add("animate__backInDown")
+        h2EligeNumeroDeJugadoresPorEquipo.classList.add("elije_numero_jugadores_por_equipo")
+        h2EligeNumeroDeJugadoresPorEquipo.innerHTML="Escriba el numero de jugadores por equipo: "
+
         setTimeout(()=>{
             document.querySelector(".blur").appendChild(inputNumeroDeJugadores)
             inputNumeroDeJugadores.classList.add("animate__backInUp")
             inputNumeroDeJugadores.classList.add("input_numero_jugadores_equipos")
+
+            document.querySelector(".blur").appendChild(input_numero_jugadores_por_equipo)
+            input_numero_jugadores_por_equipo.classList.add("animate__backInUp")
+            input_numero_jugadores_por_equipo.classList.add("input_numero_jugadores_por_equipo")
 
             document.querySelector(".blur").appendChild(botonSiguiente)
             botonSiguiente.classList.add("animate__backInUp")
@@ -215,22 +237,23 @@ botonEquipos.addEventListener("click",()=>{
 
             botonSiguiente.addEventListener("click",()=>{
                 resultado = parseInt(inputNumeroDeJugadores.value)
-                if(isNaN(inputNumeroDeJugadores.value) || inputNumeroDeJugadores.value <= 0){
-                    noEsNumero = true
+                confirmacion1 = false
+                resultado2 = parseInt(input_numero_jugadores_por_equipo.value)
+                numeroDeEquipos = resultado / resultado2
+                if(isNaN(input_numero_jugadores_por_equipo.value) || input_numero_jugadores_por_equipo.value <= 0 || resultado2 > resultado){
                     document.querySelector(".blur").appendChild(alerta)
-                    alerta.classList.add("alerta_equipos")
-                    alerta.innerHTML ="Por favor, introduzca un valor valido"
-                }  else{
-                    if(confirm(`El numero de jugadores es de ${resultado}, ¿estas seguro de que quieres continuar?`)){
-                        inputNumeroDeJugadores.classList.replace("animate__backInUp","animate__backOutRight")
-                        h2EligeNumeroDeJugadores.classList.replace("animate__backInDown","animate__backOutUp")
-                        botonSiguiente.classList.replace("animate__backInUp","animate__backOutRight")
-                        alerta.remove()
-                        setTimeout(()=>{
-                            inputNumeroDeJugadores.remove()
-                            h2EligeNumeroDeJugadores.remove()
-                            botonSiguiente.remove()
-                        },250)
+                    alerta.classList.add("alerta_equipos2")
+                    alerta.innerHTML="Por favor, completa los campos con valores validos, tambien ten en cuenta el numero de jugadores"
+                } else{
+                    if(confirm(`Confirmacion: El numero de jugadores es de ${resultado}`)){
+                        if(confirm(`Tambien, el numero de jugadores por equipo es de ${resultado2}, y se crearian ${numeroDeEquipos} equipo/s ¿estas seguro de continuar?`)){
+                            h2EligeNumeroDeJugadoresPorEquipo.classList.replace("animate__backInDown","animate__backOutUp")
+                            h2EligeNumeroDeJugadores.classList.replace("animate__backInDown","animate__backOutUp")
+                            inputNumeroDeJugadores.classList.replace("animate__backInUp","animate__backOutDown")
+                            input_numero_jugadores_por_equipo.classList.replace("animate__backInUp","animate__backOutDown")
+                            botonSiguiente.classList.replace("animate__backInUp","animate__backOutRight")
+                            alerta.remove()
+                        }
                     }
                 }
             })
